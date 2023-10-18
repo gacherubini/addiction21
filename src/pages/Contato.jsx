c142bf135a633d478711c62b4774b8d35c36389d
Gabriel <bielcheeeeee@gmail.com>
2023-10-18 17:53:11 -0300

    contato adicionado funcionando por emailjs

--- src/pages/Contato.jsx
+++ src/pages/Contato.jsx
@@ -1,13 +1,64 @@
-import React , { useEffect, useState } from "react";
-import Constants from "../Constants";
+import React , { useRef } from "react";
+import 'semantic-ui-css/semantic.min.css';
+import emailjs from '@emailjs/browser';
+
 
 function Contato() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_85dnmda', 'template_58geimq', e.target, 'tRzMuMPJLupg7ySmv')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.target.reset()
+  };
+
   return (
-    <div className="div-pagina-contato">
-      <div>
-            <h1>Contato</h1>
-            <p> Nos envie uma mensagem!</p>
-          </div> 
+    <div className='ContactForm'>
+      <h1>Contato</h1>
+      <form ref={form} onSubmit={sendEmail} className="col">
+      <label>Name</label>
+      <input type="text" name="user_name" className='input'/>
+      <label>Email</label>
+      <input type="email" name="user_email" className='input'/>
+      <label>Message</label>
+      <textarea name="message" className='input-message'/>
+      <button className='input-button'> Submit </button>
+
+    </form>
+    
+           {/* <form ref={form} onSubmit={sendEmail}>
+                  <div className='col'>
+                    <input
+                      className='input'
+                      type='text'
+                      name="user_name"
+                      placeholder='Name'
+                    ></input>
+                  </div>
+                  <div className='col'>
+                    <input
+                      type='email'
+                      name="user_email"
+                      className='input'
+                      placeholder='Email address'
+                    ></input>
+                  </div>
+                  <div className='col'>
+                    <textarea
+                      rows={3}
+                      name='message'
+                      className='input-message'
+                      placeholder='Message'
+                    ></textarea>
+                  </div>
+                <button className='input-button'> Submit </button>
+              </form> */}
     </div>
   );
 }
