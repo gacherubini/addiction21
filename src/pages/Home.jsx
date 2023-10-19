171eee4f48631d4c9c9842c9039165c198ffd404
Gabriel <bielcheeeeee@gmail.com>
2023-10-18 22:40:11 -0300

    inicio do sobre no Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -36,6 +36,17 @@ function Home() {
 
   return (
     <div className="div-pagina-home">
+      <div className="sobreNosHome">
+       <h1>Sobre Nos</h1>
+        <p>
+        A Addiction21 é uma label natural de Porto Alegre, BR (🇧🇷), que visa fortalecer a cena da música eletrônica na capital e região, com a promoção de eventos e artistas. Você escutará aqui sets e mixes dos nossos residentes e de diversos outros DJ's reconhecidos na indústria da música eletrônica. Acompanhe cada história musical atrelada à identidade de cada DJ apresentado.
+        </p>
+        <div>
+        <img className="imagemSobre" src="public/festaImagem.jpg" width="500px"/>
+        <img className="imagemSobre" src="public/records.png" width="500px"/>
+        </div>
+        
+      </div>
         <div>
           <h1>Addicast</h1>
           <p>O Addicast é um projeto da Addiction 21 que visa fortalecer a cena da música eletrônica na capital e região, com a promoção de eventos
