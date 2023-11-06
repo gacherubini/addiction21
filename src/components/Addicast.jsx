7e34fcc747ed16e821b11c8901e3975ce41bb71f
Guilherme Abreu Cherubini <guilherme_cherubini@ip-172-22-164-236.sa-east-1.compute.internal>
2023-11-06 17:36:44 -0300

    Componente addicast

---
+++ src/components/Addicast.jsx
@@ -0,0 +1,32 @@
+function Addicast(props) {
+  return (
+      <div class="col-xs-6 col-sm-3 col-lg-2 addicast">
+        <div class="addicast-content">
+          <img className="addicast-content-image" src={props.imageUrl} alt="Imagem do item da lista do Addicast"/>
+          <p className="addicast-content-title"> {props.title} </p>
+          <p className="addicast-content-artist"> {props.artist} </p>
+          <p className="addicast-content-label"> {props.label} </p>
+        </div>
+      </div>
+    )
+  }
+  
+  
+  export default Addicast
+
+
+
+  // class Addicast extends React.Component {
+  //   render() {
+  //     return <div class="col-xs-6 col-sm-3 col-lg-2 addicast">
+  //     <div class="addicast-content">
+  //       <img className="addicast-content-image" src="https://geo-media.beatport.com/image_size/250x250/feb30713-acae-443b-8c60-1cc7a9b73044.jpg" alt="Imagem do item da lista do Addicast"/>
+  //       <p className="addicast-content-title"> title </p>
+  //       <p className="addicast-content-artist"> artist </p>
+  //       <p className="addicast-content-label"> label </p>
+  //     </div>
+  //   </div>;
+  //   }
+  // }
+
+
