646cbb47ae5679bdda3cecb1b8c8d912ef345051
Gabriel <bielcheeeeee@gmail.com>
2023-11-06 15:52:51 -0300

    alteracoes na home e tentativa de boostrap

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,12 +10,13 @@ function Home() {
   const [shows, setShows] = useState([]); // State to store the fetched data
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  
+  const [musicas, setMusicasData] = useState([]); // State to store the fetched data
+
   useEffect(() => {
-    fetch('http://localhost:3000/show')
+    fetch('http://localhost:3000/musica')
     .then(response => response.json())
       .then(data => {
-        setShows(data)
+        setMusicasData(data)
         setLoading(false);
       })
       .catch(error => {
@@ -36,37 +37,39 @@ function Home() {
 
   return (
     <div className="div-pagina-home">
-      <div>
-       <h1>Addiction</h1>
-        <p>
-        A Addiction21 é uma label natural de Porto Alegre, BR (🇧🇷), que visa fortalecer a cena da música eletrônica na capital e região, com a promoção de eventos e artistas. Você escutará aqui sets e mixes dos nossos residentes e de diversos outros DJ's reconhecidos na indústria da música eletrônica. Acompanhe cada história musical atrelada à identidade de cada DJ apresentado.
-        </p> 
-        <div>
-      </div>
-          <h1>Addicast</h1>
-          <p>O Addicast é um projeto da Addiction 21 que visa fortalecer a cena da música eletrônica na capital e região, com a promoção de eventos
-             e artistas. Você escutará aqui sets e mixes dos nossos residentes e de diversos outros DJ's 
-             reconhecidos na indústria da música eletrônica. 
-             /n/n
-             Acompanhe cada história musical atrelada à identidade de cada DJ apresentado.</p>
-          <AddicastSoundCloudIFrame/>
-          <h1>Shows</h1>
-          </div>
-            {shows.map((show) => (
-             <ul>
+      <h1>Addiciton21</h1>
+      <AddicastSoundCloudIFrame/>
+        <ul>
+            {musicas.map((musica) => (
               <li>
-                <div className="div-itens-show">
-                  <h2>{show.nome}</h2> 
-                  <p>Artista: {show.artistas}</p>
-                  <p>Data: {show.data}</p>
-                </div>
+                      <a  href={musica.link}>
+                      <img 
+                    className="imagemMusicaInternaHome"
+                     src={musica.imagem}
+                   />   
+              </a> 
+                  <h4 className="nome-interno">{musica.nome}</h4> 
+                  <p>Artista: {musica.artistas}</p>
+                  <p>Data: {musica.data}</p>
               </li>
-              </ul>
-            ))} 
+            ))}
+          </ul>
     </div>
     
     )
   }
   
-
+  
   export default Home
+  
+  {/* {shows.map((show) => (
+   <ul>
+    <li>
+      <div className="div-itens-show">
+        <h2>{show.nome}</h2> 
+        <p>Artista: {show.artistas}</p>
+        <p>Data: {show.data}</p>
+      </div>
+    </li>
+    </ul>
+  ))}  */}
